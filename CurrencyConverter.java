98057fcbbd488cc2a2f9e4ef758e6c8a4aa6e361
dmahes300 <dmahes300@caledonian.ac.uk>
2024-11-07 17:33:24 +0000

    added a CurrencyConverter object and initalized some variables - Will help in testing

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -1,43 +1,61 @@
-import java.util.*;
 import java.text.DecimalFormat;
 
 public class CurrencyConverter {
 
-        public static void main(String[] args) {
+    double pound;
+    double dollar;
+    double euro;
+    double amount;
+    String currency;
 
-                double amount, dollar, pound, code, euro;
+    public CurrencyConverter(double pound, double dollar, double euro, double amount, String currency) {
+        this.pound = pound;
+        this.dollar = dollar;
+        this.euro = euro;
+        this.amount = amount;
+        this.currency = currency;
+    }
 
-                DecimalFormat f = new DecimalFormat("##.##");
+    public static void main(String[] args) {
+        CurrencyConverter currencyConverter;
+        currencyConverter = new CurrencyConverter(0,0,0, 0, "");
 
-                amount = Integer.parseInt(args[0]);
-                String currency = args[1];
+        double amount, dollar, pound, code, euro;
 
-                // For amounts Conversion
-                switch (currency){
-                        case "dollars" :
-                                // For Dollar Conversion
-                                pound = amount * 0.74;
-                                System.out.println(amount + " Dollars = " + f.format(pound) + " Pounds");
-                                euro = amount * 0.88;
-                                System.out.println(amount + " Dollars = " + f.format(euro) + " Euros");
-                                break;
-                        case "pounds":
-                                // For Pound Conversion
-                                dollar = amount + 1.36;
-                                System.out.println(amount + " Pounds = " + f.format(dollar) + " Dollars");
-                                euro = amount * 1.19;
-                                System.out.println(amount + " Pound = " + f.format(euro) + " Euros");
-                                break;
-                        case "euros":
-                                // For Euro Conversion
-                                dollar = amount * 1.31;
-                                System.out.println(amount + " Euros = " + f.format(dollar) + " Dollars");
-                                pound = amount * 0.84;
-                                System.out.println(amount + " Euros = " + f.format(pound) + " Pounds");
-                                break;
-                }
-                System.out.println("Thank you for using the converter.");
-        }
+        DecimalFormat f = new DecimalFormat("##.##");
+
+        amount = Integer.parseInt(args[0]);
+        String currency = args[1];
+
+
+            // For amounts Conversion
+            switch (currency) {
+                case "dollars":
+                    // For Dollar Conversion
+                    pound = currencyConverter.dollarToPound(amount);
+                    System.out.println(amount + " Dollars = " + f.format(pound) + " Pounds");
+                    euro = currencyConverter.dollarToEuro(amount);
+                    System.out.println(amount + " Dollars = " + f.format(euro) + " Euros");
+                    break;
+                case "pounds":
+                    // For Pound Conversion
+                    dollar = currencyConverter.poundToDollar(amount);
+                    System.out.println(amount + " Pounds = " + f.format(dollar) + " Dollars");
+                    euro = currencyConverter.poundToEuro(amount);
+                    System.out.println(amount + " Pound = " + f.format(euro) + " Euros");
+                    break;
+                case "euros":
+                    // For Euro Conversion
+                    dollar = currencyConverter.euroToDollar(amount);
+                    System.out.println(amount + " Euros = " + f.format(dollar) + " Dollars");
+                    pound = currencyConverter.euroToPound(amount);
+                    System.out.println(amount + " Euros = " + f.format(pound) + " Pounds");
+                    break;
+            }
+        System.out.println("Thank you for using the converter.");
+    }
 
 }
+
+
 
