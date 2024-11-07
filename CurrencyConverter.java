dce9b56c7c8c175834fd9036406a83521de5a8d4
dmahes300 <dmahes300@caledonian.ac.uk>
2024-11-07 17:39:02 +0000

    Added 6 functions to do the calculations

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -16,6 +16,25 @@ public class CurrencyConverter {
         this.currency = currency;
     }
 
+    public double dollarToPound(double amount){
+        return amount * 0.74;
+    }
+    public double dollarToEuro(double amount){
+        return amount * 0.88;
+    }
+    public double poundToDollar(double amount){
+        return amount + 1.36;
+    }
+    public double poundToEuro(double amount){
+        return amount * 1.19;
+    }
+    public double euroToDollar(double amount){
+        return amount * 1.31;
+    }
+    public double euroToPound(double amount){
+        return amount * 0.84;
+    }
+
     public static void main(String[] args) {
         CurrencyConverter currencyConverter;
         currencyConverter = new CurrencyConverter(0,0,0, 0, "");
@@ -26,7 +45,6 @@ public class CurrencyConverter {
 
         amount = Integer.parseInt(args[0]);
         String currency = args[1];
-
 
             // For amounts Conversion
             switch (currency) {
@@ -56,6 +74,5 @@ public class CurrencyConverter {
     }
 
 }
-
 
 
